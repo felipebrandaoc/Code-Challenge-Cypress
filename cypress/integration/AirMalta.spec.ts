1a26334788f78e43621eb3dd2eae07db078ff2ce
felipebrandaoc <felipebrandaoc@hotmail.com>
2022-07-19 12:00:13 -0300

    Update AirMalta.spec.ts

--- cypress/integration/AirMalta.spec.ts
+++ cypress/integration/AirMalta.spec.ts
@@ -1,6 +1,15 @@
 describe('AirMalta Airline', () => {
 
-    it('Get First Available Flight', () => {
+    it(`Get First Available Flight
+        Action: The user visits Air Malta  website,
+                Selects one trip type,
+                Type the Flight's origin,
+                Type the Flight's destination,
+                Sets Departure's date,
+                Search for the flights.
+        Result: The website shows up all the flights close to that date.
+                Our application shows the first available flight.
+        `, () => {
         cy.visit('/')
         cy.url().should('contains', 'airmalta.com')
         cy.selectTrip('One way')
